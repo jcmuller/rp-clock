4ff9f665dd36464920f67f5c9e4c11c4056c129d
Juan C. Müller <jcmuller@gmail.com>
2011-11-15 19:17:57 -0500

    Nice changes.

--- polar_clock.rb
+++ polar_clock.rb
@@ -6,16 +6,22 @@ class PolarClock < Processing::App
   attr_accessor :app_size, :fps
 
   def setup
-    @app_size = 300
+    @app_size = 200
     reset!
     #setting up a variable, since this is never exact
-    @fps = 60
+    @fps = 30
     frame_rate fps
     color_mode HSB, 1.0
     stroke_cap SQUARE
     smooth
     @count = 0
-    text_font load_font("HelveticaNeue-32.vlw")
+    #text_font load_font("HelveticaNeue-32.vlw")
+    text_font create_font("HelveticaNeue-Light", app_size/8.5)
+    text_align RIGHT
+
+    @s_w   = text_width("s")
+    @m_w   = text_width("m")
+    @h_w   = text_width("h")
   end
 
   def reset!
@@ -46,12 +52,19 @@ class PolarClock < Processing::App
     render_section @radius / 3.0 * 2.0, m, 60, s, 60
     render_section @radius,             s, 60, @count % @fps, @fps
 
-    t = "#{sprintf "%02d", h}h#{sprintf "%02d", m}m#{sprintf "%02d", s}s"
-    text(t, width - text_width(t) - 10, height - 10)
+    render_text h, m, s
   end
 
   def stroke_color r
-    stroke(r.to_f / @radius.to_f, 1, 0.75)
+    stroke *color_for_radius(r)
+  end
+
+  def fill_color r
+    fill *color_for_radius(r)
+  end
+
+  def color_for_radius r
+    [r.to_f / @radius.to_f, 1, 0.75]
   end
 
   def draw_marks
@@ -78,16 +91,40 @@ class PolarClock < Processing::App
     end
   end
 
-  def   render_section   r,   s,   max,   other   =    0,    other_max    =    1
-    stroke_color r
+  def render_section  r, s, max, other = 0, other_max = 1
     base_angle = -Math::PI / 2.0
+    circ = 2.0 * Math::PI
+    angle = 0
 
-    a = easeInOutQuad(other.to_f, 0.to_f, other.to_f / other_max.to_f, other_max.to_f)
-    angle = (s.to_f / max.to_f) * Math::PI * 2.0 + a * 2.0 * Math::PI / max.to_f
+    if s == 0
+      angle = circ - easeInOutQuad(other.to_f, 0.0, circ - circ / max.to_f, other_max.to_f)
+      alpha = 1 - easeInOutQuad(other.to_f, 0.0, 1.0, other_max.to_f)
+      stroke r.to_f / @radius.to_f, 1, 0.75, alpha
+    else
+      a = easeInOutQuad(other.to_f, 0.0, other.to_f / other_max.to_f, other_max.to_f)
+      angle = (s.to_f / max.to_f) * circ + a * circ / max.to_f
+      stroke_color r
+    end
 
     stroke_weight @radius / 4.0
     no_fill
     arc width/2.0, height/2.0, 2.0*r,  2.0*r,  base_angle,  angle  +  base_angle
   end
 
+  def render_text h, m, s
+    fill 0.3, 0.3, 0.3
+
+    s_n_w = text_width(sprintf "%02d", s)
+    m_n_w = text_width(sprintf "%02d", m)
+    text("s", width - 10                              , height - 10)
+    text("m", width - 10 - @s_w - s_n_w               , height - 10)
+    text("h", width - 10 - @s_w - s_n_w - @m_w - m_n_w, height - 10)
+
+    fill_color @radius
+    text((sprintf "%02d", s), width - 10 - @s_w                              , height - 10)
+    fill_color @radius / 3.0 * 2.0
+    text((sprintf "%02d", m), width - 10 - @s_w - s_n_w - @m_w               , height - 10)
+    fill_color @radius / 3.0
+    text((sprintf "%02d", h), width - 10 - @s_w - s_n_w - @m_w - m_n_w - @h_w, height - 10)
+  end
 end
